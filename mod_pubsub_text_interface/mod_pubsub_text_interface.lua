e7de5f267e7d9d4a38e5d6c5ba5479a1970045d2
Kim Alvefur <zash@zash.se>
2020-11-05 21:51:48 +0100

    mod_pubsub_text_interface: Remove receipt handling (thanks meaz)
    This apparenly breaks Gajim, which seems to stop processing of the reply
    message after noting the receipt.

--- mod_pubsub_text_interface/mod_pubsub_text_interface.lua
+++ mod_pubsub_text_interface/mod_pubsub_text_interface.lua
@@ -86,10 +86,6 @@ module:hook("message/host", function (event)
 	end
 	reply:reset();
 
-	if stanza:get_child("request", "urn:xmpp:receipts") then
-		reply:tag("received", { xmlns = "urn:xmpp:receipts", id = stanza.attr.id }):up();
-	end
-
 	if stanza:get_child("no-copy", "urn:xmpp:hints") then
 		reply:tag("no-copy", { xmlns = "urn:xmpp:hints" }):up();
 	end
