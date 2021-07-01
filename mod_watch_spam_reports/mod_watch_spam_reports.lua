87d8903dc1c0d61260f34fede8fef3f6fdc64717
Martin Dosch <martin@mdosch.de>
2021-07-01 21:00:03 +0200

    mod_watch_spam_reports: Support spam_reporting/abuse-report
    Now it also notifies for spam_reporting/abuse-report, before it only
    did for spam_reporting/spam-report

--- mod_watch_spam_reports/mod_watch_spam_reports.lua
+++ mod_watch_spam_reports/mod_watch_spam_reports.lua
@@ -7,7 +7,18 @@ module:depends("spam_reporting")
 
 module:hook("spam_reporting/spam-report", function(event)
 	local reporter_bare_jid = jid.bare(event.stanza.attr.from)
-	local report = reporter_bare_jid.." reported "..event.jid.." as spammer: "..event.reason
+	local report = reporter_bare_jid.." reported spam from "..event.jid..": "..event.reason
+	for admin_jid in admins
+		do
+			module:send(st.message({from=host,
+			type="chat",to=admin_jid},
+			report));
+		end
+end)
+
+module:hook("spam_reporting/abuse-report", function(event)
+	local reporter_bare_jid = jid.bare(event.stanza.attr.from)
+	local report = reporter_bare_jid.." reported abuse from "..event.jid..": "..event.reason
 	for admin_jid in admins
 		do
 			module:send(st.message({from=host,
