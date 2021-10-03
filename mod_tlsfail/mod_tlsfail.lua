b89e16b7083a7ab2ef6317c32d419fdb6736845d
Kim Alvefur <zash@zash.se>
2021-10-03 17:04:05 +0200

    mod_tlsfail: Add c2s support

--- mod_tlsfail/mod_tlsfail.lua
+++ mod_tlsfail/mod_tlsfail.lua
@@ -5,6 +5,11 @@ local starttls_attr = { xmlns = xmlns_starttls };
 local s2s_feature = st.stanza("starttls", starttls_attr);
 local starttls_failure = st.stanza("failure", starttls_attr);
 
+module:hook("stream-features", function(event)
+	local features = event.features;
+	features:add_child(s2s_feature);
+end);
+
 module:hook("s2s-stream-features", function(event)
 	local features = event.features;
 	features:add_child(s2s_feature);
@@ -13,7 +18,7 @@ end);
 -- Hook <starttls/>
 module:hook("stanza/urn:ietf:params:xml:ns:xmpp-tls:starttls", function(event)
 	local origin = event.origin;
-	origin.sends2s(starttls_failure);
+	(origin.sends2s or origin.send)(starttls_failure);
 	origin:close();
 	return true;
 end);
