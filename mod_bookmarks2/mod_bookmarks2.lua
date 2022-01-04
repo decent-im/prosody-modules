89fb4e76b7bec3b07180dbc11d3bd3cca0aef9cf
Kim Alvefur <zash@zash.se>
2022-01-05 00:06:24 +0100

    Back out ad7767a9f3ea
    Turns out it does error on 0.11 because it tries to compare a number
    with the string "max"
    
    Thanks Marcel

--- mod_bookmarks2/mod_bookmarks2.lua
+++ mod_bookmarks2/mod_bookmarks2.lua
@@ -20,7 +20,7 @@ local default_options = {
 	["access_model"] = "whitelist";
 };
 
-if not mod_pep.check_node_config(nil, nil, default_options) then
+if not pcall(mod_pep.check_node_config, nil, nil, default_options) then
 	-- 0.11 or earlier not supporting max_items="max" trows an error here
 	module:log("debug", "Setting max_items=pep_max_items because 'max' is not supported in this version");
 	default_options["max_items"] = module:get_option_number("pep_max_items", 256);
