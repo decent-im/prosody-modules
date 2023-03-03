ae00c366eb22d534a34097168128f4af5bee3982
Kim Alvefur <zash@zash.se>
2023-03-03 21:45:05 +0100

    mod_http_oauth2: Comment on mutation by other module

--- mod_http_oauth2/mod_http_oauth2.lua
+++ mod_http_oauth2/mod_http_oauth2.lua
@@ -141,6 +141,7 @@ function response_type_handlers.code(params, granted_jid)
 	local redirect_uri = get_redirect_uri(client, params.redirect_uri);
 	if redirect_uri == "urn:ietf:wg:oauth:2.0:oob" then
 		-- TODO some nicer template page
+		-- mod_http_errors will set content-type to text/plain if it catches this event
 		local response = { status_code = 200; headers = { content_type = "text/plain" } }
 		response.body = module:context("*"):fire_event("http-message", {
 			response = response;
