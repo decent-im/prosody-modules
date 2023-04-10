8b7fbd7126e4ff76f0b03a8b21407ff81e5da15e
Kim Alvefur <zash@zash.se>
2023-04-10 10:47:51 +0200

    mod_http_oauth2: Add some debug logging for UserInfo endpoint

--- mod_http_oauth2/mod_http_oauth2.lua
+++ mod_http_oauth2/mod_http_oauth2.lua
@@ -750,10 +750,12 @@ local function handle_userinfo_request(event)
 	local request = event.request;
 	local credentials = get_request_credentials(request);
 	if not credentials or not credentials.bearer_token then
+		module:log("debug", "Missing credentials for UserInfo endpoint: %q", credentials)
 		return 401;
 	end
-	local token_info = tokens.get_token_info(credentials.bearer_token);
+	local token_info,err = tokens.get_token_info(credentials.bearer_token);
 	if not token_info then
+		module:log("debug", "UserInfo query failed token validation: %s", err)
 		return 403;
 	end
 	-- TODO check that they actually have access to the userinfo endpoint, aka
