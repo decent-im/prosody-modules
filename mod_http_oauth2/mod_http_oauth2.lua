c085f20b487084dd5ba7801871f2b704899d1d85
Kim Alvefur <zash@zash.se>
2023-05-12 11:11:38 +0200

    mod_http_oauth2: Reference grant by id instead of value
    Fixes that the grant got mutated on use of refresh token, notably it
    would gain 'id' and 'jid' properties set there by mod_tokenauth.
    Previously also the secret token that we should not be remembering.

--- mod_http_oauth2/mod_http_oauth2.lua
+++ mod_http_oauth2/mod_http_oauth2.lua
@@ -216,12 +216,9 @@ local function new_access_token(token_jid, role, scope_string, client, id_token,
 	else
 		-- Grant exists, reuse existing refresh token
 		refresh_token = refresh_token_info.token;
-		refresh_token_info.token = nil; -- Prevent persistence of *secret* token
-
-		refresh_token_info.grant = nil; -- Prevent reference loop
 	end
 
-	local access_token, access_token_info = tokens.create_token(token_jid, grant, role, default_access_ttl, "oauth2");
+	local access_token, access_token_info = tokens.create_token(token_jid, grant.id, role, default_access_ttl, "oauth2");
 
 	local expires_at = access_token_info.expires;
 	return {
