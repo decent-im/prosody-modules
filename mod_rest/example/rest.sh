0d215322601c66c8494da25b571da4a4d83f93b7
Kim Alvefur <zash@zash.se>
2023-10-01 16:39:48 +0200

    mod_rest/rest.sh: Restore default read-only behavior and the -rw flag

--- mod_rest/example/rest.sh
+++ mod_rest/example/rest.sh
@@ -13,6 +13,8 @@
 HOST=""
 DOMAIN=""
 
+SESSION="session-read-only"
+
 if [ -f "${XDG_CONFIG_HOME:-$HOME/.config}/restrc" ]; then
 	# Config file can contain the above settings
 	source "${XDG_CONFIG_HOME:-$HOME/.config}/restrc"
@@ -23,7 +25,7 @@ if [ -f "${XDG_CONFIG_HOME:-$HOME/.config}/restrc" ]; then
 fi
 
 if [[ $# == 0 ]]; then
-	echo "${0##*/} [-h HOST] [/path] kind=(message|presence|iq) ...."
+	echo "${0##*/} [-h HOST] [-rw] [/path] kind=(message|presence|iq) ...."
 	# Last arguments are handed to HTTPie, so refer to its docs for further details
 	exit 0
 fi
@@ -33,6 +35,12 @@ if [[ "$1" == "-h" ]]; then
 	shift 2
 elif [ -z "${HOST:-}" ]; then
 	HOST="$(hostname)"
+fi
+
+if [[ "$1" == "-rw" ]]; then
+	# To e.g. save Accept headers to the session
+	SESSION="session"
+	shift 1
 fi
 
 if [[ "$HOST" != *.* ]]; then
@@ -55,4 +63,4 @@ if [[ "$1" == /* ]]; then
 	shift 1
 fi
 
-https --check-status -p b --session rest -A oauth2 -a "$HOST" --oauth2-scope "$SCOPE" "$HOST/rest$GET_PATH" "$@"
+https --check-status -p b --"$SESSION" rest -A oauth2 -a "$HOST" --oauth2-scope "$SCOPE" "$HOST/rest$GET_PATH" "$@"
