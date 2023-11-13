4516dc708e6383bed6a6fd5eec01fc66a6b875c4
Kim Alvefur <zash@zash.se>
2023-11-13 17:14:09 +0100

    mod_audit_auth: Include hostpart with audit events
    mod_audit seems to expect this to be JIDs, not bare usernames.

--- mod_audit_auth/mod_audit_auth.lua
+++ mod_audit_auth/mod_audit_auth.lua
@@ -1,16 +1,18 @@
+local jid = require"util.jid";
+
 module:depends("audit");
 -- luacheck: read globals module.audit
 
 module:hook("authentication-failure", function(event)
 	local session = event.session;
-	module:audit(session.sasl_handler.username, "authentication-failure", {
+	module:audit(jid.join(session.sasl_handler.username, module.host), "authentication-failure", {
 		session = session,
 	});
 end)
 
 module:hook("authentication-success", function(event)
 	local session = event.session;
-	module:audit(session.sasl_handler.username, "authentication-success", {
+	module:audit(jid.join(session.sasl_handler.username, module.host), "authentication-success", {
 		session = session,
 	});
 end)
