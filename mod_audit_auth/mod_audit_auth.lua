8a1548ebdb6cedb2e1ea9909158b3a254b3ac217
Matthew Wild <mwild1@gmail.com>
2023-12-01 11:34:52 +0000

    mod_audit_auth: Ignore FAST authentication events by default
    FAST is more like a cookie that allows linking new connections to a previous
    (e.g. password) authentication. Since we assume that FAST tokens are secure
    (not user generated) and not shareable, it reduces a lot of noise by filtering
    out uninteresting authentication events.

--- mod_audit_auth/mod_audit_auth.lua
+++ mod_audit_auth/mod_audit_auth.lua
@@ -3,6 +3,8 @@ local jid = require"util.jid";
 module:depends("audit");
 -- luacheck: read globals module.audit
 
+local only_passwords = module:get_option_boolean("audit_auth_passwords_only", true);
+
 module:hook("authentication-failure", function(event)
 	local session = event.session;
 	module:audit(jid.join(session.sasl_handler.username, module.host), "authentication-failure", {
@@ -12,6 +14,9 @@ end)
 
 module:hook("authentication-success", function(event)
 	local session = event.session;
+	if only_passwords and session.sasl_handler.fast then
+		return;
+	end
 	module:audit(jid.join(session.sasl_handler.username, module.host), "authentication-success", {
 		session = session,
 	});
