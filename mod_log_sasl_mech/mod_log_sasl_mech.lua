5a243e5a813cffdfb21a605a5433742627aa98bf
Kim Alvefur <zash@zash.se>
2023-12-04 21:38:27 +0100

    mod_log_sasl_mech: Handle auth event from other than mod_saslauth
    E.g. mod_http_oauth2

--- mod_log_sasl_mech/mod_log_sasl_mech.lua
+++ mod_log_sasl_mech/mod_log_sasl_mech.lua
@@ -1,6 +1,7 @@
 
 module:hook("authentication-success", function (event)
 	local session = event.session;
-	local sasl_handler = session.sasl_handler;
-	session.log("info", "Authenticated with %s", sasl_handler and sasl_handler.selected or "legacy auth");
+	local sasl_handler = session and session.sasl_handler;
+	local log = session and session.log or module._log
+	log("info", "Authenticated with %s", sasl_handler and sasl_handler.selected or "legacy auth");
 end);
