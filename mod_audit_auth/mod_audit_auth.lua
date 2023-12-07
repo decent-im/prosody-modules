481f15acb70060d5389b76f4eecb62fd54668673
Matthew Wild <mwild1@gmail.com>
2023-12-07 13:02:36 +0000

    mod_audit_auth: Improve user-agent building (fixes traceback)

--- mod_audit_auth/mod_audit_auth.lua
+++ mod_audit_auth/mod_audit_auth.lua
@@ -27,9 +27,21 @@ module:hook("client_management/new-client", function (event)
 	local session, client = event.session, event.client;
 
 	local client_info = st.stanza("client", { id = client.id });
+
 	if client.user_agent then
-		client_info:text_tag("agent", client.user_agent);
+		local user_agent = st.stanza("user-agent", { xmlns = "urn:xmpp:sasl:2" })
+		if client.user_agent.software then
+			user_agent:text_tag("software", client.user_agent.software, { id = client.user_agent.software_id; version = client.user_agent.software_version });
+		end
+		if client.user_agent.device then
+			user_agent:text_tag("device", client.user_agent.device);
+		end
+		if client.user_agent.uri then
+			user_agent:text_tag("uri", client.user_agent.uri);
+		end
+		client_info:add_child(user_agent);
 	end
+
 	if client.legacy then
 		client_info:text_tag("legacy");
 	end
